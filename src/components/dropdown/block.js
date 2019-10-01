175481962bf062e67f999c411607a62b9921ab0a
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-10-01 19:27:51 +0300

    Close dropdown when focus changed to another

--- src/components/dropdown/block.js
+++ src/components/dropdown/block.js
@@ -3,12 +3,15 @@ var OPENNED_CLASSNAME = 'Dropdown--is-openned'
 function addBlurHandler (target, dropdown) {
   var listener = target.addEventListener('blur', function (event) {
     if (!dropdown) { dropdown = event.target }
-    var classes = event.relatedTarget? event.relatedTarget.classList : []
-    for (var i = 0; i < classes.length; i++) {
-      if (classes[i].indexOf('Dropdown') === 0) {
-        target.removeEventListener('blur', listener)
-        addBlurHandler(event.relatedTarget, dropdown)
-        return
+    if (event.relatedTarget) {
+      var parentNode = event.relatedTarget.parentNode
+      while (parentNode) {
+        if (parentNode === dropdown) {
+          target.removeEventListener('blur', listener)
+          addBlurHandler(event.relatedTarget, dropdown)
+          return
+        }
+        parentNode = parentNode.parentNode
       }
     }
     dropdown.classList.remove(OPENNED_CLASSNAME)
@@ -18,15 +21,12 @@ function addBlurHandler (target, dropdown) {
 
 document.addEventListener('DOMContentLoaded', function () {
   var elements = document.getElementsByClassName('Dropdown')
-
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i]
-
     element.addEventListener('focus', function (event) {
       var Dropdown = event.target
       Dropdown.classList.add(OPENNED_CLASSNAME)
     })
-
     addBlurHandler(element, null)
   }
 })
