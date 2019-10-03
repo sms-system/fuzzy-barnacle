e5c6b59b5e27dba807ddf3a2369d7a98fe4c10f9
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-10-03 10:39:40 +0300

    Dropdowns on mobile theme

--- src/components/dropdown/block.js
+++ src/components/dropdown/block.js
@@ -1,10 +1,12 @@
-var OPENNED_CLASSNAME = 'Dropdown--is-openned'
+const OPENNED_CLASSNAME = 'Dropdown--is-openned'
+const BODY_MOBILE_THEME_CLASSNAME = 'Theme--mobile'
+const BODY_LOCKED_CLASSNAME = '__locked'
 
 function addBlurHandler (target, dropdown) {
-  var listener = target.addEventListener('blur', function (event) {
+  const listener = target.addEventListener('blur', (event) => {
     if (!dropdown) { dropdown = event.target }
     if (event.relatedTarget) {
-      var parentNode = event.relatedTarget.parentNode
+      let parentNode = event.relatedTarget.parentNode
       while (parentNode) {
         if (parentNode === dropdown) {
           target.removeEventListener('blur', listener)
@@ -14,19 +16,42 @@ function addBlurHandler (target, dropdown) {
         parentNode = parentNode.parentNode
       }
     }
+    const bodyClasses = document.body.classList
+    if (bodyClasses.contains(BODY_MOBILE_THEME_CLASSNAME)) {
+      bodyClasses.remove(BODY_LOCKED_CLASSNAME)
+    }
     dropdown.classList.remove(OPENNED_CLASSNAME)
+    delete dropdown.dataset.isOpenned
     target.removeEventListener('blur', listener)
   })
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elements = document.getElementsByClassName('Dropdown')
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i]
-    element.addEventListener('focus', function (event) {
-      var Dropdown = event.target
-      Dropdown.classList.add(OPENNED_CLASSNAME)
-    })
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.getElementsByClassName('Dropdown')
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i]
+    let timer = {}
+    element.addEventListener('focus', ((element, timer) => () => {
+      // If wasn't click event for 0.8ms after focus, set openned state
+      // Its adds side feature — long tap for dropdown content preview
+      timer.isOpenned = setTimeout(() => element.dataset.isOpenned = true, 800)
+
+      element.classList.add(OPENNED_CLASSNAME)
+
+      const bodyClasses = document.body.classList
+      if (bodyClasses.contains(BODY_MOBILE_THEME_CLASSNAME)) {
+        bodyClasses.add(BODY_LOCKED_CLASSNAME)
+      }
+    })(element, timer))
+    element.addEventListener('click', ((element, timer) => () => {
+      // If no openned state, prevent closing
+      if (!element.dataset.isOpenned) {
+        if (timer.isOpenned) { clearTimeout(timer.isOpenned) }
+        element.dataset.isOpenned = true
+        return
+      }
+      element.blur()
+    })(element, timer))
     addBlurHandler(element, null)
   }
 })
