f23a12cadc88b3194c4921af65e13718169ad811
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-10-07 15:09:39 +0300

    Fixed code splitting

--- components/pages/routes.js
+++ components/pages/routes.js
@@ -1,11 +1,24 @@
+import React from 'react'
 import imported from 'react-imported-component'
 
+import REPO_VIEW from './repo-page-view-types'
+
 const Home = imported(() => import('./repo-list'))
-const RepoPage = imported(() => import('./repo-page'))
+const FileList = imported(() => import('./file-list'))
+const NotFound = imported(() => import('./404'))
 
 import PAGES from './page-names'
 
 export default {
   [PAGES.REPO_LIST]: { path: '/', component: Home },
-  [PAGES.REPO_PAGE]: { path: '/:repo/:view_type?/:branch?/:path*', component: RepoPage }
+  [PAGES.REPO_PAGE]: { path: '/:repo/:view_type?/:branch?/:path*', component: ({ params }) => {
+    if (!params.view_type) {
+      return <FileList { ...params } />
+    }
+    switch (params.view_type) {
+      case REPO_VIEW.TREE: return <FileList { ...params } />
+      case REPO_VIEW.BLOB: return <div>Blob</div>
+      default: return <NotFound />
+    }
+  } }
 }
