7bb237d3cd226227421b210076ac526259ad5d9d
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-10-26 00:07:12 +0300

    one more ts

--- packages/webapp/data-sources/file-content.ts
+++ packages/webapp/data-sources/file-content.ts
@@ -4,7 +4,9 @@ import fetch from 'node-fetch'
 //@ts-ignore
 import { API_URL } from '../generated/config.json'
 
-export default async ({ repo, branch = 'master', path }) => {
+type repoPos = { repo: string, branch: string, path: string }
+
+export default async ({ repo, branch = 'master', path }: repoPos) => {
   let url = `${API_URL}/repos/${repo}/blob/${branch}/${path || ''}`
   const data = await fetch(url)
   const res = await data.text()
